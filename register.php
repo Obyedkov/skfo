cd32c63510d4e9ed1386faa611bfd7cf7a3aca26
Obyedkov <vratar1001@gmail.com>
2023-02-28 20:24:44 +0300

    Запрос базы данных написал и поставил класс ошибки

--- register.php
+++ register.php
@@ -2,13 +2,46 @@
 include 'dbconn.php';
 include 'helpers.php';
 
-$conn = new mysqli($servername, $username, $password, $database);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+$con = new mysqli($servername, $username, $password, $database);
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
 }
-$conn->set_charset("utf8");
+$con->set_charset("utf8");
 
 $errors = [];
 
-$mainContent = include_template('register.php');
+if (!empty($_POST)) {
+    $formArrays = [
+        'email',
+        'name',
+        'password',
+    ];
+
+    foreach ($formArrays as $formArray) {
+        if ($formArray === 'email') {
+            if (filter_var($_POST[$formArray], FILTER_VALIDATE_EMAIL) === false) {
+                $errors[$formArray] = "Вы не ввели email";
+            }
+        }
+        if (empty($_POST[$formArray])) {
+            $errors[$formArray] = "Поле не заполнено";
+        }
+    }
+
+    if(empty($errors)){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+        $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+        $addRegister = "INSERT INTO `register` ( `name`, `email`, `password`) VALUE ('$name','$email', '$password')";
+        if (mysqli_query($con, $addRegister)) {
+            echo "выполнено запрос";
+        }
+    }
+
+}
+
+$mainContent = include_template('register.php', [
+    'errors' => $errors,
+]);
 echo include_template('layout.php', ['title' => 'Регистрация', 'content' => $mainContent]);
--- templates/register.php
+++ templates/register.php
@@ -4,10 +4,19 @@
         <i class='bx bxl-graphql'></i>
     </div>
     <h2>Регистрация</h2>
-    <form action="/register.php" class="form" method="POST">
-        <input type="text" placeholder="Имя">
-        <input type="email" placeholder="Email">
-        <input type="password" placeholder="Пароль">
+    <form action="/register.php" class="form" method="POST" autocomplete="off">
+        <?php $classname = isset($errors['name']) ? "form__input--error" : ""; ?>
+        <input class="<?= $classname ?>" name="name" id="name" value="" type="text" placeholder="Введите имя">
+        <p class="form__message"><?= $errors['name'] ?? ""; ?></p>
+
+        <?php $classname = isset($errors['email']) ? "form__input--error" : ""; ?>
+        <input class="<?= $classname ?>" name="email" id="email" value="" type="email" placeholder="Введите e-mail">
+        <p class="form__message"><?= $errors['email'] ?? ""; ?></p>
+
+        <?php $classname = isset($errors['password']) ? "form__input--error" : ""; ?>
+        <input class="<?= $classname ?>" name="password" id="password" value="" type="password" placeholder="Пароль">
+        <p class="form__message"><?= $errors['password'] ?? ""; ?></p>
+
         <button type="submit"> Зарегистрироваться</button>
     </form>
     <section class="content__side">
